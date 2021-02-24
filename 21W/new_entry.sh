dbdda8d30eb0b52918fe4d45d16a5bd70c9ab447
arodriguez22 <alexrodriguez282@gmail.com>
2021-02-24 17:00:33 -0500

    new log

--- 21W/new_entry.sh
+++ 21W/new_entry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo -e "Alex Rodriguez\nLocalization Project\n\ngoal:\nobjective\nstatus:\nreport:" > $(date +"%Y%m%d").log
+echo -e "Alex Rodriguez\nLocalization Project\n\ngoal:\nobjective:\nstatus:\nreport:" > $(date +"%Y%m%d").log
 
